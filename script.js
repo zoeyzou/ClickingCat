2870e00ffef4fb4a1d2c2b43a58acb311da830d0
zoeyzou <zoeyzou2010@hotmail.com>
2018-04-29 15:14:02 +0200

    Init Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$(".alert").alert('close');
+
+//adding event listener for image clicking and reset button
+var counter = 0;
+
+$(document).ready(function() {
+
+  $("#clickable-img").click(function() {
+    counter++;
+    if (counter === 100) {
+      alert("Dude, you should take a rest!");
+      counter = 0;
+    }
+
+    $("#text-area").text(counter);
+
+  });
+
+  $("body").on("click", "#my-btn", function() {
+    counter = 0;
+
+    $("#text-area").text(counter);
+  });
+
+});
+
